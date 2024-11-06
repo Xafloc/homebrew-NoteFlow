6482bb7441fdae1b75c3e96a77cf5025ed37040e
darren-aims <darren@aims.to>
2024-11-05 22:44:43 -0500

    Adding noteflow formula

--- noteflow.rb
+++ noteflow.rb
@@ -4,7 +4,7 @@ class Noteflow < Formula
   desc "NoteFlow is a lightweight, Markdown-based note-taking application with task management capabilities. It provides a clean interface for creating, viewing, and managing notes with support for tasks, images, and code snippets."
   homepage "https://github.com/Xafloc/NoteFlow"
   url "https://github.com/Xafloc/NoteFlow/archive/refs/tags/v0.1.tar.gz"
-  sha256 "497752421a3da0947c3032a0ec72a7c5b3805cc57e70a22abf07b59bf167431a"
+  sha256 "20bc3a4dc8c50453861e62b6192c5ff99cef1c676741ba7331494c768b876e23"
 
   depends_on "python@3.9"
 
